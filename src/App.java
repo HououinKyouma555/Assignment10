a858e95f57ccf73ee816d0994cf94c4ea0de6616
HououinKyouma555 <130073365+HououinKyouma555@users.noreply.github.com>
2024-02-13 14:43:38 -0500

    java program

--- src/App.java
+++ src/App.java
@@ -1,22 +1,62 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Random rand = new Random();
-        int numbers[] = new int[1000];
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Input size of array:");
+        int n = scnr.nextInt();
+        scnr.close();
+        int numbers[] = new int[n];
 
         for (int i = 0; i < numbers.length; i++)
         {
-            numbers[i] = rand.nextInt(1000);
+            numbers[i] = rand.nextInt(n);
         }
 
+        int[] copyNumbers = new int[numbers.length];
+
+        for (int x : numbers)
+        {
+            copyNumbers[x] = numbers[x];
+        }
+        
+        System.out.println("Beginning Merge Sort");
         System.out.println("Numbers before sorting:");
         printArray(numbers);
-
+        long time1 = System.nanoTime();
         mergeSort(numbers);
 
         System.out.println("Numbers after sorting:");
         printArray(numbers);
+        long time2 = System.nanoTime();
+        long mergeTime = time2 - time1;
+        System.out.println("Merge Sort time taken: " + mergeTime + " ns");
+
+        System.out.println("Beginning Bubble Sort");
+        System.out.println("Numbers before sorting:");
+        printArray(copyNumbers);
+        time1 = System.nanoTime();
+        bubbleSort(numbers);
+
+        System.out.println("Numbers after sorting:");
+        printArray(copyNumbers);
+        time2 = System.nanoTime();
+        long bubTime = time2 - time1;
+        System.out.println("Bubble Sort time taken: " + bubTime + " ns");
+
+        System.out.println("\nMerge Sort time: " + mergeTime);
+        System.out.println("Bubble Sort time: " + bubTime);
+
+        if (mergeTime < bubTime)
+        {
+            System.out.println("Merge sort was faster by " + (bubTime - mergeTime) + " ns");
+        }
+        else
+        {
+            System.out.println("Bubble sort was faster by " + (mergeTime - bubTime) + " ns");
+        }
     }
 
     public static void printArray(int[] num){
@@ -94,4 +134,18 @@ public class App {
         }
     }
 
+    public static void bubbleSort(int[] inputArray){
+        for (int i = 0; i < inputArray.length; i++)
+        {
+            for (int j = 1; j < (inputArray.length-1); j++)
+            {
+                if (inputArray[j-1] > inputArray[j])
+                {
+                    int temp = inputArray[j-1];
+                    inputArray[j-1] = inputArray[j];
+                    inputArray[j] = temp;
+                }
+            }
+        }
+    }
 }
